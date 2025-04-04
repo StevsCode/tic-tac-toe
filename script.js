0b3e2bc4c5798bfddbb5c0a268bb89a3690a0634
StevsCode <stevensavarese1@gmail.com>
2025-04-04 15:29:26 +0200

    Create ganeboard, player and gamecontroller to switch turns, make a move, check if there a tie or a win and make you reset the game

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Gameboard Factory
 const Gameboard = (() => {
     let board = ["", "", "", "", "", "", "", "", ""];
 
@@ -18,6 +19,62 @@ const Gameboard = (() => {
     return { getBoard, setCell, resetBoard };
 })();
 
+// Player Factory
 const Player = (symbol) => {
     return { symbol };
 };
+
+// GameController function
+function GameController() {
+    let currentPlayer = Player("X");
+    let gameOver = false;
+
+    function switchTurn() {
+        currentPlayer = (currentPlayer.symbol === "X") ? Player("O") : Player("X");
+    }
+
+    function makeMove(index) {
+        if (gameOver) return;
+
+        if (Gameboard.setCell(index, currentPlayer.symbol)) {
+            const winner = checkWinner();
+            if (winner) {
+                console.log(winner === "Tie" ? "It's a tie!" : `${winner} wins!`);
+                gameOver = true;
+            } else {
+                switchTurn(); 
+            }
+        }
+    }
+
+    function checkWinner() {
+        const board = Gameboard.getBoard();
+
+        for (let i = 0; i < 3; i++) {
+            if (board[i * 3] && board[i * 3] === board[i * 3 + 1] && board[i * 3 + 1] === board[i * 3 + 2]) {
+                return board[i * 3]; 
+            }
+        }
+        
+        for (let i = 0; i < 3; i++) {
+            if (board[i] && board[i] === board[i + 3] && board[i + 3] === board[i + 6]) {
+                return board[i];
+            }
+        }
+
+        if (board[0] && board[0] === board[4] && board[4] === board[8]) {
+            return board[0]; 
+        }
+        if (board[2] && board[2] === board[4] && board[4] === board[6]) {
+            return board[2]; 
+        }
+
+        if (!board.includes("")) {
+            return "Tie";
+        }
+
+        return null; 
+    }
+
+    return { makeMove, resetGame: Gameboard.resetBoard };
+}
